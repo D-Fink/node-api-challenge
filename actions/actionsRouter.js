6e6437b1a61aa66c9430dc66c0a3ea4c1c9a7419
D-Fink <dfink4badjuju@gmail.com>
2020-01-17 10:52:11 -0700

    CRUD methods tested - actions post needs work

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const actionsHub = require('../data/helpers/actionModel.js');
 
 router.post('/', (req, res) => {
+    console.log(req.body)
     if(req.body.description === '' || req.body.notes === ''){
         return res.status(400).json({message: 'please include a description and notes'})
     }
@@ -16,7 +17,7 @@ router.post('/', (req, res) => {
         res.status(500).json({message: 'error adding action'})
     })
 })
-
+//ok
 router.get('/', (req, res) => {
     actionsHub.get()
     .then(actions => {
@@ -26,7 +27,7 @@ router.get('/', (req, res) => {
         res.status(500).json({message: 'error loading actions'})
     })
 })
-
+//ok
 router.put('/:id', (req, res) => {
     if(req.body.description === '' || req.body.notes === ''){
         return res.status(400).json({message: 'please include description and notes'})
@@ -43,12 +44,12 @@ router.put('/:id', (req, res) => {
         res.status(500).json({message: 'error updating action'})
     })
 })
-
+//ok
 router.delete('/:id', (req, res) => {
     actionsHub.remove(req.params.id)
     .then(action => {
-        if(action < 0){
-            return res.status(404).json({message: 'could not find action'})
+        if(action === 0){
+            res.status(404).json({message: 'could not find action'})
         }else{
             res.status(200).json({message: 'action was deleted'})
         }
@@ -57,5 +58,9 @@ router.delete('/:id', (req, res) => {
         res.status(500).json({message: 'error deleting action'})
     })
 })
+
+// function validateProjectId(req, res, next){
+//     projectHub.get()
+// }
 
 module.exports = router;
